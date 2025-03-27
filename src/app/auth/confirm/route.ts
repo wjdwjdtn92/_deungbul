f3319b3228ea2d3a172f951eda1cdf8ec47defa6
감자 <wjdwjdtn92@naver.com>
2025-03-27 13:21:19 +0900

    refactor: @/lib/utils 경로를 @/shared/lib/utils로 수정

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,6 +1,6 @@
 import { type EmailOtpType } from '@supabase/supabase-js';
 import { type NextRequest } from 'next/server';
-import { createClient } from '@/shared/api/supabase';
+import { createClient } from '@/shared/lib/supabase/server';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: NextRequest) {
